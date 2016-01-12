9d86023722b0af0f449f128fb3818ca7cba35d56
Soumya <gsoumya@cse.iitk.ac.in>
2016-01-12 23:25:00 +0530

    Added header

---
+++ asgn1/src/lexforpy.h
@@ -0,0 +1,11 @@
+/**
+ * Header for lex input
+ * Define tokens	
+ */
+#define OP_ASSIGN 1
+#define IDENTIFIER 2
+#define LIT_INT_CONST 3
+#define LIT_STRING 4
+#define KEY_PRINT 5
+// #define INDENT 6
+// #define DEDENT 7 
