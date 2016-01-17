1bf33fb5469eca6c5abe8de63f1b64731449f9da
Soumya <gsoumya@cse.iitk.ac.in>
2016-01-17 22:19:01 +0530

    can be submitted, needs improvement

--- asgn1/src/lexforpy.c
+++ asgn1/src/lexforpy.c
@@ -1,20 +1,70 @@
 #include "lexforpy.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 extern int yylex();
 extern int yylineno;
 extern char* yytext;
 extern FILE *yyin;
 
-char *names[]={NULL};
-
+int token_count[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+char *token_name[]={"KEYWORD","OP_MATH","OP_REL","OP_LOGIC",
+"OP_BIT","PARENTHESIS","PUNCTUATOR","ASSIGN_MATH","ASSIGN_BIT",
+"LIT_INT","LIT_FLOAT","LIT_IMAGINARY","LIT_BOOL","LIT_STR",
+"LIT_NONE","IDENTIFIER"};
 int main(int argc,char **argv){
-	int ntoken;
+	int tokenid;
+	int i;
 	FILE *fh;
 	if(argc==2&&(fh=fopen(argv[1],"r")))
 		yyin=fh;
-	while(ntoken=yylex()){
-		printf("%d\n", ntoken);
+	FILE *f_intermediate[16];
+	for (i = 0; i < 16; ++i)
+	{
+		char filename[20];
+		sprintf(filename,"count%d.txt",i);
+		f_intermediate[i]=fopen(filename,"w");
+	}
+	while(tokenid=yylex()){
+		if(tokenid>0)
+		{
+			//printf("%s\n",yytext);
+			token_count[tokenid-1]++;
+			fprintf(f_intermediate[tokenid-1], "%s\n",yytext);
+		}	
+		else if(tokenid==-1)
+		{
+			printf("Line %d - Unknown stream of characters : %s\n", yylineno,yytext);
+			return 0;
+		}
+	}
+	printf("      Token      Occurances        Lexemes\n");
+	printf("____________________________________________\n");	
+	for (i = 0; i < 16; ++i)
+	{
+		char filename[20];
+		fclose(f_intermediate[i]);
+		sprintf(filename,"count%d.txt",i);
+		f_intermediate[i]=fopen(filename,"r");
+	}
+	for (i = 0; i < 16; ++i)
+	{
+		ssize_t read;
+		size_t len=0;
+		char *line=NULL;
+		if(token_count[i]>0){
+			printf("%11s%12d",token_name[i],token_count[i]);	
+			while(getline(&line,&len,f_intermediate[i])!=-1)
+			{
+				printf("%20s\n                       ", line);
+			}
+			printf("\n");
+		}	
+		fclose(f_intermediate[i]);
+		char filename[20];
+		sprintf(filename,"count%d.txt",i);
+		remove(filename);
 	}
 	return 0;
 }
--- asgn1/src/lexforpy.h
+++ asgn1/src/lexforpy.h
@@ -2,10 +2,28 @@
  * Header for lex input
  * Define tokens	
  */
-#define OP_ASSIGN 1
-#define IDENTIFIER 2
-#define LIT_INT_CONST 3
-#define LIT_STRING 4
-#define KEY_PRINT 5
-// #define INDENT 6
-// #define DEDENT 7 
+#define KEYWORD 1
+
+#define OP_MATH 2
+#define OP_REL 3
+#define OP_LOGIC 4
+#define OP_BIT 5
+// #define OP_MEMBER 6
+// #define OP_IDENTITY 7
+
+#define PARENTHESIS 6
+#define PUNCTUATOR 7
+#define ASSIGN_MATH 8
+#define ASSIGN_BIT 9
+
+#define LIT_INT 10
+#define LIT_FLOAT 11	
+// #define LIT_IMAGINARY 12
+#define LIT_BOOL 13
+#define LIT_STR 14
+#define LIT_NONE 15
+
+#define IDENTIFIER 16
+
+// #define INDENT 17
+// #define DEDENT 18
